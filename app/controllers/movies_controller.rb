6a10d2e6c43bd0a873e83930da4249a334e89c5f
Mitchell Walker <mitchellwwalker@gmail.com>
2018-09-20 22:24:43 +0000

    part 3 almost complete - code cleanup

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,13 +12,17 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.ratings
-    @sort = params[:sort_by]
-    @rating_params = params[:ratings]
-    @checked = @rating_params ? @rating_params.keys : @all_ratings
-    @checked.each {|rating| params[rating] = 'checked'}
-    
-    @movies = Movie.where(:rating => @checked).order(@sort) 
-    
+    @sort = params[:sort_by] || session[:sorted]
+    set_style @sort
+    session[:rating_params] = session[:rating_params] ||  all_ratings_hash
+    @params = params[:ratings].nil? ? session[:rating_params] : params[:ratings] 
+    session[:rating_params] = @params
+    session[:sorted] = @sort
+    @movies = Movie.where(:rating => @params.keys).order(session[:sorted]) 
+    if (( params[:sort_by].nil? and !session[:sorted].nil? ) or (params[:ratings].nil? and !session[:rating_params].nil?))
+      flash.keep
+      redirect_to movies_path(:sort_by => session[:sorted],:ratings => @params)
+    end
   end
 
   def new
@@ -48,5 +52,14 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-
+  
+  def all_ratings_hash arr
+    hash = Hash.new
+    arr.each {|e| hash[e] = 1 }
+  end
+  
+  def set_style sort
+    @style_sort_by = "hilite" if sort == 'title'
+    @style_ratings = "hilite" if sort == 'release_date'
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ActiveRecord::Base
   def self.ratings
-    Movie.order(:rating).select(:rating).distinct.inject([]){ |ratings_list, movie| ratings_list.push movie.rating}
+    #@test = Movie.order(:rating).select(:rating).distinct
+    Movie.distinct.pluck(:rating).sort
+    #Movie.order(:rating).select(:rating).distinct.inject([]){ |ratings_list, movie| ratings_list.push movie.rating}
   end
 end
